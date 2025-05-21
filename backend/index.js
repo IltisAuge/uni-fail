874b48ae4ec7e15cebe685e7d47cf19733734b0b
IltisAuge <danielziegler.1@gmx.de>
2025-05-21 18:33:01 +0200

    Changed indentation to 4 tabs, added proxy.conf.json for development

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+const express = require('express');
+
+const server = express();
+server.get('/api', (req, res) => {
+    res.header('Content-Type', 'text/plain');
+    res.send('Welcome on the /api endpoint!');
+});
+
+server.get('/api/hello', (req, res) => {
+    res.header('Content-Type', 'text/plain');
+    res.send('Hello World!');
+});
+
+server.listen(5010, () => {
+    console.log('Server listening on port 5010');
+})
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	imports: [RouterOutlet],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+	title = 'frontend';
+
+	constructor(private http: HttpClient) {
+		http.get('/api', {responseType: 'text'}).subscribe(data => {
+			console.log(data);
+		})
+	}
 }
