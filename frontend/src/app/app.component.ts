1ef0d41b60e9724eee81187f28003241fadd79ac
datlissschen <lisa.na2710@gmail.com>
2025-05-23 16:24:17 +0200

    added home component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {HttpService} from './http.service';
+import {HomeComponent} from './home/home.component';
 
 @Component({
 	selector: 'app-root',
-	imports: [RouterOutlet],
+	imports: [RouterOutlet, HomeComponent],
 	templateUrl: './app.component.html',
 	standalone: true,
 	styleUrl: './app.component.css'
