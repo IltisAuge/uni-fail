20753218db875325ed291b29d4e10fafb1e885ce
datlissschen <lisa.na2710@gmail.com>
2025-05-30 15:56:03 +0200

    adding the empty pages and adding the routing. Routing has a bug. if you uncomment the header.component.ts, needs work

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,15 +1,22 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
+
+import {SearchComponent} from './search/search.component';
+import {RankingComponent} from './ranking/ranking.component';
+import {PostFormComponent} from './post-form/post-form.component';
+import {AboutUsComponent} from './about-us/about-us.component';
+import {AboutUserComponent} from './about-user/about-user.component';
+
+
 export const routes: Routes = [
 	{ path: '', component: HomeComponent },
-	// Füge hier weitere Routen hinzu, wenn du mehr Seiten hast
 
-	// hier weitere Routen während der Entwicklung hinzu, wenn mehr Seiten hinzugefügt werden
-	/*{ path: 'search', component:  },
-	{ path: 'ranking', component:  },
-	{ path: 'Post', component:  },
-	{ path: 'AboutUs', component:  },*/
+	{ path: 'Search', component: SearchComponent },
+	{ path: 'Ranking', component: RankingComponent },
+	{ path: 'Post', component: PostFormComponent },
+	{ path: 'AboutUs', component: AboutUsComponent },
+	{ path: 'MyAccount', component: AboutUserComponent },
 ];
 
 
