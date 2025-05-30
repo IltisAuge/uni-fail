991a4d955b9215fb50b264179bc1326737688157
datlissschen <lisa.na2710@gmail.com>
2025-05-30 15:56:03 +0200

    adding the empty pages and adding the routing. Routing has a bug. if you uncomment the header.component.ts, needs work

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+//import { CommonModule } from '@angular/common';
+//import { RouterOutlet } from '@angular/router';
+//import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+	selector: 'app-header',
+	standalone: true,
+	imports: [
+		//CommonModule,
+		//RouterOutlet,
+		//RouterLink,
+		//RouterLinkActive
+	],
+	templateUrl: './header.component.html',
+	styleUrl: './header.component.css'
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
