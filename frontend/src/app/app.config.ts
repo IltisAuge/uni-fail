b6a3240665e5ae8434b76d9de5bc87f0032b1572
IltisAuge <danielziegler.1@gmx.de>
2025-06-26 22:31:52 +0200

    Added debug code to check for outgoing CSRF token header

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,18 +3,24 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideClientHydration} from '@angular/platform-browser';
-import {provideHttpClient, withXsrfConfiguration} from "@angular/common/http";
+import {HttpInterceptorFn, provideHttpClient, withInterceptors, withXsrfConfiguration} from "@angular/common/http";
 import {provideAnimations} from '@angular/platform-browser/animations';
+
+export const logCsrfInterceptor: HttpInterceptorFn = (req, next) => {
+    const token = req.headers.get('X-XSRF-TOKEN');
+    console.log('Outgoing CSRF Token Header:', token);
+    return next(req);
+};
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideZoneChangeDetection({eventCoalescing: true}),
 		provideRouter(routes),
 		provideClientHydration(),
-		provideHttpClient(withXsrfConfiguration({
-            cookieName: 'XSRF-TOKEN',
-            headerName: 'X-XSRF-TOKEN'
-        })),
+        provideHttpClient(
+            withXsrfConfiguration({ cookieName: 'XSRF-TOKEN', headerName: 'X-XSRF-TOKEN' }),
+            withInterceptors([logCsrfInterceptor])
+        ),
         provideAnimations()
 	]
 };
