d4e35737abfa5c54731bbe07be9a39b3342299bd
IltisAuge <danielziegler.1@gmx.de>
2025-06-29 16:52:10 +0200

    Added csrf.interceptor.ts

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,6 +4,7 @@ import {provideClientHydration} from '@angular/platform-browser';
 import {HttpInterceptorFn, provideHttpClient, withInterceptors, withXsrfConfiguration} from '@angular/common/http';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {routes} from './app.routes';
+import {csrfInterceptor} from './csrf.interceptor';
 
 export const logCsrfInterceptor: HttpInterceptorFn = (req, next) => {
     const token = req.headers.get('X-XSRF-TOKEN');
@@ -16,13 +17,7 @@ export const appConfig: ApplicationConfig = {
         provideZoneChangeDetection({eventCoalescing: true}),
         provideRouter(routes),
         provideClientHydration(),
-        provideHttpClient(withXsrfConfiguration({
-            cookieName: 'XSRF-TOKEN',
-            headerName: 'X-XSRF-TOKEN',
-            crossOrigin: true,
-        } as any),
-        withInterceptors([logCsrfInterceptor]),
-        ),
+        provideHttpClient(withInterceptors([csrfInterceptor, logCsrfInterceptor])),
         provideAnimations(),
     ],
 };
