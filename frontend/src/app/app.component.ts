24d2ab40b34ca79c5a402445f05049b073a7ef9c
datlissschen <lisa.na2710@gmail.com>
2025-07-03 13:21:23 +0200

    changing the go back application for a customizable return

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import {RouterLink, RouterOutlet, Router, NavigationEnd} from '@angular/router';
 import {isPlatformBrowser, NgClass, NgTemplateOutlet} from '@angular/common';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 import {faArrowLeft, faCircleInfo, faMoon, faLightbulb} from '@fortawesome/free-solid-svg-icons';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../environments/environment';
 import {NavigationComponent} from './navigation/navigation.component';
+import {NavigationService} from '../services/navigation.service';
+
 
 @Component({
     selector: 'app-root',
@@ -25,7 +27,9 @@ export class AppComponent implements OnInit {
 
     constructor(
         @Inject(PLATFORM_ID) private platformId: Object,
-        private http: HttpClient) {
+        private http: HttpClient,
+        private router: Router,
+        private navigationService: NavigationService) {
     }
 
     ngOnInit() {
